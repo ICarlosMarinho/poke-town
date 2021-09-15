0e3f08f7218493d7a824f1264e5d42b24f7f591f
Carlos Marinho <carlos.marinho20@outlook.com>
2021-09-15 10:34:37 -0300

    Add responsive styles

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -5,4 +5,9 @@ export const FooterContainer = styled(HeaderContainer)`
   grid-area: footer;
   border-bottom: none;
   border-top: 5px solid ${(props) => (props.darkMode ? "#fff" : "#000")};
+
+  @media (max-width: 500px) {
+    flex-direction: row;
+    height: 70px;
+  }
 `;
